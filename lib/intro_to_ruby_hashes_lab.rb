5cf930267388b971863c9430c637b4a0ac4b658f
Reed Roffis <reedroffis18@gmail.com>
2020-12-01 03:06:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  
+  {location: 'New York City', occupation: 'Student'}
+    => {:location=>"New York City", :occupation=>"Cashier"}
 end
 
 def update_counting_hash(hash, key)
